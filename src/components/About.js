b59c0fc30954bb85aeb007569ea1b6eeceec1e81
Kugler, Lisa (THOUGHTWORKS DEUTSCHLAND GMBH) <Lisa.Kugler@otto.de>
2018-10-21 12:43:45 +0200

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div className="About">
+                <h1>Wir sind Emma und Paula und wir LIEBEN Bücher!</h1>
+            </div>
+        );
+    }
+}
+
+export default About;
