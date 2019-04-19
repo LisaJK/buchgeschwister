2f3c11c6ee2da47c7b86ec566c9c9c309045bc30
Kugler, Lisa <Lisa.Kugler@gmail.com>
2019-04-19 11:27:26 +0200

    tests fixed

--- src/components/Book.test.js
+++ src/components/Book.test.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import Book from './Book';
+import PropTypes from "prop-types";
 
 describe("should render a book", () => {
 
+    const book = shallow(<Book
+                    title={'the_title'}
+                    author={'the_author'}
+                    age={'12'}
+                    fontSize={'8'}
+                    content={'the_content'}
+                    genre={'genre'}
+                    plus={'the_plus'}
+                    minus={'the_minus'}
+                    awards={'the_awards'}
+                    stars={'5'}
+                    price={12.50}
+                    similarTo={'the_similar'}
+    />);
+
     it('should have title', function () {
-        const book = shallow(<Book title={'the_title'}/>);
         expect(book.find('MainInfo').props().title).toEqual('the_title');
     });
 
     it('should have author', function () {
-        const book = shallow(<Book author={'the_author'}/>);
         expect(book.find('MainInfo').props().author).toEqual('the_author');
     });
 
-    it('should have img', function () {
-        const book = shallow(<Book img={'the_src'}/>);
-        expect(book.find('MainInfo').props().img).toEqual('the_src');
-    });
-
     it('should have content', function () {
-        const book = shallow(<Book content={'the_content'}/>);
         expect(book.find('span').text()).toEqual('the_content');
     });
 
     it('should have age', function () {
-        const book = shallow(<Book age={'12'}/>);
         expect(book.find('AdditionalInfo').props().age).toEqual('12');
     });
 });
